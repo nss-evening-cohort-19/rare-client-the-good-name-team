fcd2d4f2c689de69fbb1b3ebe0ba7f5a486e2157
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2022-11-07 21:25:57 -0600

    added nav link, api, and category page

---
+++ pages/categories/allCategories.js
@@ -0,0 +1,29 @@
+/* import { React, useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import { getAllCategories } from '../../api/categoryData';
+
+export default function CategoriesPage() {
+ const [category, setCategory] = useState([]);
+}
+
+const getCategories = () => {
+  getAllCategories((category.id).then(setCategory));
+};
+
+useEffect(() => {
+  getCategories();
+ }, []);
+
+return (
+  <>
+    <h2>Categories</h2>
+    <Table striped bordered hover>
+      <thread>
+        <tr>
+            <th></th>
+        </tr>
+      </thread>
+    </Table>
+
+  </>
+); */
