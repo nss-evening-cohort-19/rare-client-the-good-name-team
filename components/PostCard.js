90241d478ef755863af4d9197fc0f77b1de93dde
cod-y-clark <cody66clark@gmail.com>
2022-11-12 12:02:25 -0600

    Post form created

---
+++ components/PostCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
+import { deletePost } from '../api/postData';
+
+export default function PostCard({ postObj, onUpdate }) {
+  const deleteThisPost = () => {
+    if (window.confirm(`Delete ${postObj.title}?`)) {
+      deletePost(postObj.id).then(() => {
+        onUpdate();
+        window.location.reload();
+      });
+    }
+  };
+
+  return (
+    <Card style={{ width: '25rem', margin: '5%' }}>
+      <Card.Body>
+        <div> {postObj.title}</div>
+        <Card.Img variant="top" src={postObj.image_url} alt={postObj.title} style={{ height: '400px' }} />
+        <Link href={`/posts/${postObj.id}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        <Link href={`/posts/edit/${postObj.id}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisPost} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+PostCard.propTypes = {
+  postObj: PropTypes.shape({
+    id: PropTypes.number,
+    user_id: PropTypes.number,
+    category_id: PropTypes.number,
+    title: PropTypes.string,
+    publication_date: PropTypes.string,
+    image_url: PropTypes.string,
+    content: PropTypes.string,
+    approved: PropTypes.bool,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+// import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
+import { getAllPosts } from '../api/postData';
+import PostCard from '../components/PostCard';
+
 function Home() {
-  return 'Rare App';
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    getAllPosts().then(setPosts);
+  }, []);
+
+  return (
+    <>
+      <Link href="/posts/new" passHref>
+        <Button variant="info" className="m-2">New Post</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {posts.map((post) => (
+          <PostCard postObj={post} onUpdate={getAllPosts} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
