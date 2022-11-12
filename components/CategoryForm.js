6006b13d4e011093ae1112b3e0e3328242a82ea8
Kenneth Chittaphong <kennethchittaphong@gmail.com>
2022-11-12 09:37:15 -0600

    added all category stuff

---
+++ components/CategoryForm.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { createCategory } from '../api/categoryData';
+
+const initialState = {
+  label: '',
+};
+
+// eslint-disable-next-line react/prop-types
+function CategoryForm({ categoryObj, refresh }) {
+  const [formInput, setFormInput] = useState(initialState);
+
+  useEffect(() => {
+    if (categoryObj.id) {
+      setFormInput(categoryObj);
+    }
+  }, [categoryObj]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      ...formInput,
+    };
+    console.warn(payload);
+    createCategory(payload).then(() => {
+      refresh();
+      setFormInput(initialState);
+    });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Create a New Category</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Create a New Category"
+          name="label"
+          value={formInput.label}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Create
+      </Button>
+    </Form>
+  );
+}
+
+CategoryForm.propTypes = {
+  categoryObj: PropTypes.shape({
+    id: PropTypes.number,
+    label: PropTypes.string,
+  }),
+};
+
+CategoryForm.defaultProps = {
+  categoryObj: initialState,
+};
+
+export default CategoryForm;
--- pages/categories/allCategories.js
+++ pages/categories/allCategories.js
@@ -1,29 +1,49 @@
-/* import { React, useEffect, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { React, useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { getAllCategories } from '../../api/categoryData';
+import CategoryForm from '../../components/CategoryForm';
 
-export default function CategoriesPage() {
- const [category, setCategory] = useState([]);
+export default function AllCategoriesPage() {
+  const [category, setCategory] = useState([]);
+
+  const getCategories = () => {
+    getAllCategories(category.id).then(setCategory);
+  };
+
+  useEffect(() => {
+    getCategories();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const refresh = () => getCategories();
+
+  return (
+    <>
+      <CategoryForm refresh={refresh} />
+      <h2>Categories</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th> </th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            category?.map((categories) => (
+              <tr>
+                <td> <Button>Delete</Button> <Button>Edit</Button> </td>
+                <Link href={`/posts/category/${categories.id}`} passHref>
+                  <td>{categories.label}</td>
+                </Link>
+              </tr>
+            ))
+          }
+        </tbody>
+      </Table>
+    </>
+  );
 }
-
-const getCategories = () => {
-  getAllCategories((category.id).then(setCategory));
-};
-
-useEffect(() => {
-  getCategories();
- }, []);
-
-return (
-  <>
-    <h2>Categories</h2>
-    <Table striped bordered hover>
-      <thread>
-        <tr>
-            <th></th>
-        </tr>
-      </thread>
-    </Table>
-
-  </>
-); */
